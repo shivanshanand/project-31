e461021d5226d2c6c50730b020301d73fd68b137
shivanshanand <74009335+shivanshanand@users.noreply.github.com>
2020-12-26 17:25:04 +0530

    Update particles.js

--- particles.js
+++ particles.js
@@ -1,9 +1,23 @@
-class Particles{
-    constructor(){
-        
+class Particle{
+    constructor(x,y,r){
+        var op={
+            restitution:0.5
+        }
+
+        this.body=Bodies.circle(x,y,r,op);
+
+        this.color=color( random(0,255), random(0,255), random(0,255));
+
+        World.add(world, this.body);
+        this.r=r;
     }
 
     display(){
+        push()
+        translate(this.body.position.x, this.body.position.y);
+        rotate(this.body.angle);
+        ellipseMode(RADIUS);
+        ellipse(0,0,this.r,this.r);
+        pop()
     }
-
 }
