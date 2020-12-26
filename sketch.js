14f691f49f23bf8233b012e44b0a845ae6cf2487
shivanshanand <74009335+shivanshanand@users.noreply.github.com>
2020-12-26 14:11:27 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,6 +3,9 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
+var divisions=[];
+var divisionheight=[];
+
 function setup() {
   createCanvas(480,800);
   engine = Engine.create();
@@ -11,36 +14,18 @@ function setup() {
   ground1=new Ground(220,480,480,10);
   division1=new Division(100,370,10,300);
 
-
-
-
-
-
-
-
-
-
-
-
 }
 
 function draw() {
   background(250);  
   Engine.update(engine);
 
-  
+  for(var s=0; s<=width; s=s+80){
+    divisions.push(new Division(s,height-divisionheight/2, 10, divisionheight));
+}
 
 ground1.display();
 division1.show();
-
-
-
-
-
-
-
-
-
 
 
 }
