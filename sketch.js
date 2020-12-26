6dba3d1da2bddcda1dc0b48976d19959270c2521
shivanshanand <74009335+shivanshanand@users.noreply.github.com>
2020-12-26 17:28:04 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,9 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var divisions=[];
-var divisionheight=[];
+var divisionheight=300;
+
+var particles=[];
 
 function setup() {
   createCanvas(480,800);
@@ -13,18 +15,25 @@ function setup() {
 
   ground1=new Ground(220,480,480,10);
   division1=new Divisions(100,370,10,300);
-
 }
 
 function draw() {
   background(250);  
   Engine.update(engine);
 
+  for(var s=0; s<particles.length; s++){
+    partciles[s].display
+  }
+
+if(frameCount%60===0){
+    particles.push(new Particle(random(width/2-10,width/2+10), 10, 10))
+} 
+
   for(var s=0; s<=width; s=s+80){
     divisions.push(new Divisions(s,height-divisionheight/2, 10, divisionheight));
 }
 
 ground1.display();
-
+division1.show();
 
 }
