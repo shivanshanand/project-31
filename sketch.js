2ea486176700121213d9b1e5cd04b6b90cad11c0
shivanshanand <74009335+shivanshanand@users.noreply.github.com>
2020-12-27 18:57:57 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,33 +7,69 @@ var divisions=[];
 var divisionheight=300;
 
 var particles=[];
+var plinkos=[];
+
+function preload(){
+  bgImg=loadImage("plinko bg.png")
+}
 
 function setup() {
-  createCanvas(480,800);
+  createCanvas(800,800);
   engine = Engine.create();
   world = engine.world;
 
-  ground1=new Ground(220,480,480,10);
-  division1=new Divisions(100,370,10,300);
-}
-
-function draw() {
-  background(250);  
-  Engine.update(engine);
-
-  for(var s=0; s<particles.length; s++){
-    partciles[s].display
-  }
-
-if(frameCount%60===0){
-    particles.push(new Particle(random(width/2-10,width/2+10), 10, 10))
-} 
+  ground1=new Ground(width/2,height,width,20);
+  //division1=new Divisions(100,370,10,300);
 
   for(var s=0; s<=width; s=s+80){
     divisions.push(new Divisions(s,height-divisionheight/2, 10, divisionheight));
 }
 
-ground1.display();
-division1.show();
+for(var i=30; i<=width; i=i+50){
+ plinkos.push(new Plinko(i,75,15))
+}
+
+for(var i=30; i<=width; i=i+50){
+  plinkos.push(new Plinko(i,140,15))
+ }
+ 
+for(var i=30; i<=width; i=i+50){
+  plinkos.push(new Plinko(i,205,15))
+ }
+
+ for(var i=30; i<=width; i=i+50){
+  plinkos.push(new Plinko(i,270,15))
+ }
+ 
+ for(var i=30; i<=width; i=i+50){
+  plinkos.push(new Plinko(i,335,15))
+ }
+
+}
+
+function draw() {
+  background(bgImg);  
+  Engine.update(engine);
+
+  if(frameCount%60===0){
+    particles.push(new Particle(random(100,700), 10, 10))
+} 
+
+  for(var s=0; s<particles.length; s++){
+    particles[s].display();
+  }
+
+  ground1.display();
+
+  for(var s=0; s<divisions.length; s++){
+    divisions[s].display();
+  }
+
+  for(var s=0; s<plinkos.length; s++){
+    plinkos[s].display();
+  }  
+
+
+//division1.show();
 
 }
