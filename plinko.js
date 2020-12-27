22491f13ccff1c44fc7ca3b25a8c4bdceeae4d5c
shivanshanand <74009335+shivanshanand@users.noreply.github.com>
2020-12-27 18:57:21 +0530

    Update plinko.js

--- plinko.js
+++ plinko.js
@@ -1,9 +1,24 @@
 class Plinko{
-    constructor(){
-        
+    constructor(x,y,r){
+        var op={
+            isStatic:true,
+            restitution:0.3
+        }
+
+        this.body=Bodies.circle(x,y,r,op);
+
+        this.color=color( random(0,255), random(0,255), random(0,255));
+
+        World.add(world, this.body);
+        this.r=r;
     }
 
     display(){
+        push()
+        translate(this.body.position.x, this.body.position.y);
+        rotate(this.body.angle);
+        ellipseMode(RADIUS);
+        ellipse(0,0,this.r,this.r);
+        pop()
     }
-
 }
