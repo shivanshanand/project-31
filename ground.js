bc8d20face08e2256838c6c91e00c3f8671cb8b2
shivanshanand <74009335+shivanshanand@users.noreply.github.com>
2020-12-27 18:55:52 +0530

    Update ground.js

--- ground.js
+++ ground.js
@@ -13,8 +13,8 @@ class Ground{
     display(){
         var pos=this.body.position;
         rectMode(CENTER);
-        fill("purple");
-        rect(pos.x,pos.y,this.w,this.h)
+        fill("white");
+        rect(pos.x,pos.y,this.width,this.height)
     }
 
 }
