0deea73fdd8594fb2ce111433f65ea40cd4ba09b
shivanshanand <74009335+shivanshanand@users.noreply.github.com>
2020-12-28 21:39:36 +0530

    Update particles.js

--- particles.js
+++ particles.js
@@ -6,7 +6,7 @@ class Particle{
 
         this.body=Bodies.circle(x,y,r/2,op);
 
-        this.color=color(random(0,255));
+        this.color=color(random(0,255),random(0,255),random(0,255));
 
         World.add(world, this.body);
         this.r=r;
@@ -17,6 +17,7 @@ class Particle{
         translate(this.body.position.x, this.body.position.y);
         rotate(this.body.angle);
         ellipseMode(RADIUS);
+        fill(this.color);
         ellipse(0,0,this.r,this.r);
         pop()
     }
